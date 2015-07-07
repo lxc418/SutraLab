1468ff176ea16de2cb7e302f4cb2b80d6462d857
Chenming <cmzhang1318@hotmail.com>
2015-07-07 11:04:52 +1000

    get next line is added

--- mfiles/etc/getNextLine.m
+++ mfiles/etc/getNextLine.m
@@ -1,5 +1,24 @@
-function line=getNextLine(varargin)
-% line=readnext(filehandle,'cerierion','with','')
+function o=getNextLine(varargin)
+% getNextLine get the next line of the file based on the keyword and criteria.
+% if search ends up to the end of the file, function returns -1.
+% Arguments:
+%         'keyword'   --  the keyword to search at the beginning of the line
+%         'criterion' --  Can be 'with', 'without' or 'equal'
+%                         'with'  find the line start with the keyword
+%                         'without' find the line started without the keyword
+%                         'equal' find out the NEXT line of one line that equals
+%                                 the given keyword 
+%         'operation' --  Can be 'delete' (only works when 'criterion'='with')
+%                         delete the keyword from the extracted line
+% Examples:
+% line=getNextLine(fn,'criterion','with','keyword','##','operation','delete');
+%    purpose: find line from the file handled by fn with line started with '##'.
+%    once found, delete the starting '##'
+% line=readnext(fn,'cerierion','without','#')
+%    Goal: find the first non-commented line ( with commentting notation as '#')
+%  o2.str2      =getNextLine(fn,'criterion','equal','keyword','## ');
+%    Goal: omit all lines that are same as '## ' and return the first line not
+%         the same as '## '
 % if 'ceriterion' is set as 'with'
 %   function returns to a line match with the keyword
 % if 'ceriterion' is set as 'without'
@@ -7,20 +26,62 @@ function line=getNextLine(varargin)
 % read next line that is not started from '#'
 % fn  -- file handle
 % keywords -- should be hatchk
+% TO 06/07/15 
+
 
   [fn, varargin] = getNext(varargin,'double','');
   % an option to see whether use inp contents to guide the reading process
   %   a hard reading process will be conducted if left empty
   [criterion,  varargin] = getProp(varargin,'criterion',[]);
-  [keyword,  varargin]   = getProp(varargin,'keyword',[]);
+  [keyword  ,  varargin] = getProp(varargin,'keyword',[]);
   [operation,  varargin] = getProp(varargin,'operation',[]);
   
-  if strcmp(criterion,'without')
-     line=fgetl(fn);
-     while strcmp(line(length(keyword)), keyword)
-       line=fgetl(fn);
+
+  o=fgetl(fn);
+  kw_length=length(keyword);
+  %if length(o)<kw_length
+  %  fprintf(1,'keyword length is more than extracted string length! \n');
+  %  return
+  %end
+  if strcmpi(criterion,'without')
+     while strcmp(o(1:  min(kw_length,length(o))   ), keyword)
+       o=fgetl(fn);
+       if o==-1 
+         fprintf(1,'Search up to the end of the file\n');
+	 return
+       end
+       %lth=min(kw_length,length(o))
+       %if length(o)<kw_length
+       %  fprintf(1,'keyword length is more than extracted string length! \n');
+       %  return
+       %end
      end
-  elseif strcmp(criterion,'with')
+  elseif strcmpi(criterion,'equal')
+     while ~strcmp(o,keyword)
+       o=fgetl(fn);
+       if o==-1 
+         fprintf(1,'Search up to the end of the file\n');
+	 return 
+       end
+     end
+     o=fgetl(fn);
+  elseif strcmpi(criterion,'with')
+     while ~strcmp(o(1:min(kw_length,length(o))), keyword)
+       o=fgetl(fn);
+       if o==-1 
+         fprintf(1,'Search up to the end of the file\n');
+	 return
+       end
+       %if length(o)<kw_length
+       %  %fprintf(1,'keyword length is more than extracted string length! \n');
+       %  return
+       %end
+     end
+
+     if strcmp(operation,'delete')
+       o=o(length(keyword)+1:end);
+     end
+
   end
 end % function
 
