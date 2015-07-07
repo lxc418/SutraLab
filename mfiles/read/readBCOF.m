516cb26031f5ac4d6b4dfa7d5fd5b8314466e838
Chenming <cmzhang1318@hotmail.com>
2015-07-07 15:30:32 +1000

    add readBCOF.m

--- mfiles/read/readBCOF.m
+++ mfiles/read/readBCOF.m
@@ -1,10 +1,17 @@
-function [o,o2]=readBCOP(varargin)
-  % readBCOP reads BCOP file   
-  % o  a struct the same size as the number of output.
-  % o2   a struct extracting headers with the extraction inf
+function [o,o2]=readBCOF(varargin)
+  % readBCOF reads BCOF file   
+  % 
+  % Argument: 
+  %   'outputnumber' -- the number of output wish to extract
+  %
+  % Output:
+  %   o  -- struct storing the simulation results
+  %   o2 -- struct extracting headers
   % Example:
-  % [npbcdata,nodhead]=readBCOP('project','outputnumber',3);
-  %    Purpose: parsing 'project.npbc' (or 'project.BCOP')
+  % [bcofdata,nodhead]=readBCOF('project','outputnumber',3);
+  %    Function: parsing 'project.bcof' (or 'project.BCOF')
+  %              only the first 3 outputs get extracted
+
   % a string storing the caller functions
   caller=dbstack('-completenames'); caller=caller.name;
 
@@ -15,12 +22,12 @@ function [o,o2]=readBCOP(varargin)
   [inpObj,  varargin]      = getProp(varargin,'inpObj',[]);
   o2.output_no             = output_no;
 
-  fn = fopen([fname,'.BCOP']);
+  fn = fopen([fname,'.BCOF']);
   if fn == -1
-    fprintf(1,'%s : Trying to open %s .npbc\n',caller,fname);
-    fn = fopen([fname,'.npbc']);
+    fprintf(1,'%s : Trying to open %s .bcof\n',caller,fname);
+    fn = fopen([fname,'.bcof']);
     if fn == -1
-      fprintf('%s: file npbc found!!\n',caller,fname);
+      fprintf('%s: file bcof found!!\n',caller,fname);
       o=-1;o2=-1;
       return
     end
@@ -31,7 +38,7 @@ function [o,o2]=readBCOP(varargin)
   o2.title2 = getNextLine(fn,'criterion',...
                          'with','keyword','## ','operation','delete');
 
-  % ---------------- Parsing the line with npbc, element info-----------------
+  % ---------------- Parsing the line with bcof, element info-----------------
   o2.MeshInfo = getNextLine(fn,'criterion','equal','keyword','## ');
   tmp = regexprep(o2.MeshInfo,{'#','(',')','\,','*','='},{'','','','','',''});
   tmp = textscan(tmp,'%s %s %s %f %f %f %*s %f %*s');
@@ -43,7 +50,7 @@ function [o,o2]=readBCOP(varargin)
 
   % ---------------- parsing the number of results    ------------------------
   tmp      = getNextLine(fn,'criterion','with','keyword',...
-                 '## SPECIFIED-PRESSURE NODE RESULTS','operation','delete');
+                 '## FLUID SOURCE/SINK NODE RESULTS','operation','delete');
   tmp      = textscan(tmp,'%f ');
   o2.ktprn = tmp{1};  % expected no. time steps
   if output_no ~= 0;
@@ -71,16 +78,16 @@ function [o,o2]=readBCOP(varargin)
       tmp = getNextLine(fn,'criterion','with'...
                     ,'keyword','##   Node');
 		    
-      % get o.npbc as it is not disclosed in *.npbc
+      % get o.nbcp as it is not disclosed in *.bcof
       if n==output_from 
-        [tmp o2.npbc] = getBlock(fn,'keyword','#');
-        tmp = textscan(tmp,'%f %s %s %f %f %f %f %f',o2.npbc);
+        [tmp o2.nsop] = getBlock(fn,'keyword','#');
+        tmp = textscan(tmp,'%f %s %s %f %f %f ',o2.nsop);
       else
-        tmp = textscan(fn ,'%f %s %s %f %f %f %f %f',o2.npbc);
+        tmp = textscan(fn ,'%f %s %s %f %f %f ',o2.nsop);
       end
 
-      [o(n).i,o(n).ibc,o(n).bcdstr,o(n).qpl,o(n).uucut,...
-                      o(n).qpu,o(n).pvec,o(n).pbc]= deal(tmp{:});
+      [o(n).i,o(n).ibc,o(n).notapp,o(n).qin,o(n).uucut,...
+                      o(n).qu]= deal(tmp{:});
     else
       fprintf(1,['WARNING FROM %s: Simulation is not completed\n %g'...
              ' out of %g outputs extracted\n'],caller,n,o2.ktprn);
