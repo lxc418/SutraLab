aebc715c3c11e7d3452985d0d54636cc379a804b
diablo465 <cmzhang1318@hotmail.com>
2017-06-14 10:04:35 +1000

    fixed a bug with jingling in  RelativeK_VanGenuchten1980.m

--- mfiles/misc/RelativeK_VanGenuchten1980.m
+++ mfiles/misc/RelativeK_VanGenuchten1980.m
@@ -16,6 +16,7 @@ function [kr,sw]=RelativeK_VanGenuchten1980(psim,alpha,nv,swr)
 %      kr       -- relative permeability of soil 
 %      TO20150906
 
-se   = 1./( 1+(alpha*psim).^nv   ).^(1-1/nv);
+%se   = 1./( 1+(alpha*psim).^nv   ).^(1-1/nv);
+se   = 1./( 1+(psim/alpha).^nv   ).^(1-1/nv);
 kr=se.^0.5.*(1-   (   1- se.^(nv/(nv-1) ) ).^((nv-1)/nv)   ).^2;
 sw=se*(1-swr)+swr;
