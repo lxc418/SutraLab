a9a798350eb7eece0eb887d4508b33739708bfde
xiaocheng <lxcvae@gmail.com>
2021-05-26 11:33:16 +1000

    simplify the sutravap related codes

--- mfiles/@vapinpObj/get_dz_cell_mtx.m
+++
@@ -1,6 +0,0 @@
-function o=get_dz_cell_mtx(o)
-%function o=get_dy_cell_mtx(o)
-% this subfunction gets the delta y, when meshtype is 2D and regular
-
-    o.dz_cell_mtx       = o.z_nod_mtx;
-end 
--- mfiles/@vapinpObj/get_vol.m
+++
@@ -1,7 +0,0 @@
-function o=get_dx_cell_mtx(o)
-%function o=get_dy_cell_mtx(o)
-% this subfunction gets the delta y, when meshtype is 2D and regular
-
-    o.vol_mtx       = o.dx_cell_mtx.*o.dy_cell_mtx.*o.dz_cell_mtx;
-
-end 
