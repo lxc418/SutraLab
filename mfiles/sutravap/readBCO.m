59c364a92e2a8546afc6f625ece44d62256c6217
Mr Xiaocheng Liu <s4524462@goliath.labs.eait.uq.edu.au>
2022-08-22 17:22:00 +1000

    correct the real daytime when plot et

--- mfiles/sutravap/readBCO.m
+++ mfiles/sutravap/readBCO.m
@@ -46,44 +46,7 @@ function [et,mesh]=readBCO(inp,nod)
     % the format of %g can be less than the numbers in the [  ]. 
     %in that case, the
     %data obtain will repeat using the pattern given by %g. 
-    if inp.nbcspr==1
-        output_no=length(nod)-2;
-        temp=fscanf(fn, '%g', [(inp.nn2+2) output_no]); 
-%       % here using only one %g would be enough
-%       % notice: temp here stores the evaporation rate at all time steps
-%       %, which is
-%       % important to obtain the precise
-%       % result for the accumulative evaporation. however, et1 only
-%       %stores the 
-%       % evaporation rate at perticular snapshot
-%       % this is due to too large array size.
-%       
-       et1=zeros(inp.nn2,length(nod));
-       aet1=zeros(inp.nn2,length(nod));
-       for i=1:length(nod)  % loop for each snapshot
-          if i==1
-              if nod(i).itout==0
-                et1(:,i)  = -temp(3:inp.nn2+2,nod(i+1).itout);   
-                aet1(:,i) = -temp(3:inp.nn2+2,nod(i+1).itout)*inp.scalt   ;
-              elseif nod(i).itout==1
-                et1(:,i)  = -temp(3:inp.nn2+2,nod(i).itout);
-                aet1(:,i) = -temp(3:inp.nn2+2,nod(i).itout)*inp.scalt;
-              end
-                  
-          else
-              et1(:,i)  = -temp(3:inp.nn2+2,nod(i).itout);
-              aet1(:,i) = aet1(:,i-1)- ...
-                  sum( temp(3:inp.nn2+2,nod(i-1).itout+1:nod(i).itout)  ,2) *inp.scalt;
-          end
-        et(i).steps     = nod(i).itout;
-        et(i).t_elapsed = nod(i).tout;
-        et(i).label     = {'et','aet'};
-        et(i).terms{1}  = et1(:,i)' ;
-        et(i).terms{2}  = aet1(:,i)';
-       end
-        
-    else  % when nbcspr is the same as nod
-        output_no=length(nod)-2;
+        output_no=length(nod)-1;
         temp=fscanf(fn, '%g', [(inp.nn2+2) output_no]); 
 %       % here using only one %g would be enough
 %       % notice: temp here stores the evaporation rate at all time steps
@@ -98,27 +61,21 @@ function [et,mesh]=readBCO(inp,nod)
        aet1=zeros(inp.nn2,output_no);
        for i=1:output_no  % loop for each snapshot
           if i==1
-              if nod(i).itout==0
-                et1(:,i)  = -temp(3:inp.nn2+2,i+1);   
-                aet1(:,i) = -temp(3:inp.nn2+2,i+1)*inp.scalt   ;
-              elseif nod(i).itout==1
                 et1(:,i)  = -temp(3:inp.nn2+2,i);
                 aet1(:,i) = -temp(3:inp.nn2+2,i)*inp.scalt;
-              end
-                  
+                               
           else
               et1(:,i)  = -temp(3:inp.nn2+2,i);
               aet1(:,i) = aet1(:,i-1)+ ...
                   et1(:,i)*inp.nbcspr *inp.scalt;
           end
-        et(i).steps     = nod(i).itout;
-        et(i).t_elapsed = nod(i).tout;
+        et(i).steps     = temp(1,i);
+        et(i).t_elapsed = temp(2,i);
         et(i).label     = {'et','aet'};
         et(i).terms{1}  = et1(:,i)' ;
         et(i).terms{2}  = aet1(:,i)';
        end        
         
-    end
 %       % avet1 -- the accumulated evaporation on the whole surface.
 %       %algorithm= et*area/whole area
 %       avet1=(xyf(4,1:p.nnh)*aet1(2:(p.neh+2),:))/sum(xyf(4,1:p.nnh));
